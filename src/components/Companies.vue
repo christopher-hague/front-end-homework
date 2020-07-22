a298d7201526f09b2dc2509e6fcf95a065ff2478
christopher-hague <christopher.a.hague@gmail.com>
2020-07-22 14:34:13 -0600

    CompaniesDataTable

---
+++ src/components/Companies.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Domain</th>
+        <th>Number of Employees</th>
+        <th>Subscriptions Per Employee</th>
+        <th>Total Subscriptions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="company in companies" v-bind:key="company.id">
+        <td>{{ company.id }}</td>
+        <td>
+          <router-link
+            :to="{ name: 'company',
+            params: { id: company.id, company: company }}">
+            <p>{{ company.name }}</p>
+          </router-link>
+        </td>
+        <td>{{ company.domain }}</td>
+        <td>{{ company.numberOfEmployees }}</td>
+        <td>{{ company.subscriptionsPerEmployee }}</td>
+        <td>{{ company.numberOfEmployees * company.subscriptionsPerEmployee }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ['companies'],
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,19 @@
 <template>
   <div>
     <h1 class="title">Fake Companies</h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Domain</th>
-          <th>Number of Employees</th>
-          <th>Subscriptions Per Employee</th>
-          <th>Total Subscriptions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="company in companies" v-bind:key="company.id">
-          <td>{{ company.id }}</td>
-          <td>
-            <router-link
-              :to="{ name: 'company',
-              params: { id: company.id, company: company }}">
-              <p>{{ company.name }}</p>
-            </router-link>
-          </td>
-          <td>{{ company.domain }}</td>
-          <td>{{ company.numberOfEmployees }}</td>
-          <td>{{ company.subscriptionsPerEmployee }}</td>
-          <td>{{ company.numberOfEmployees * company.subscriptionsPerEmployee }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <Companies v-bind:companies="companies" />
   </div>
 </template>
 
 <script>
 import CompanyService from '../services/CompanyService';
+import Companies from '../components/Companies.vue';
 
 export default {
   name: 'home',
-  components: {},
+  components: {
+    Companies,
+  },
   data() {
     return {
       companies: [],
