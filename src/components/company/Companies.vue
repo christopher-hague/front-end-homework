d4faf37f27b8fa4e923c6f3a23eece1e89e77e19
christopher-hague <christopher.a.hague@gmail.com>
2020-07-27 16:54:14 -0600

    get companies data

--- src/components/company/Companies.vue
+++ src/components/company/Companies.vue
@@ -6,14 +6,13 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="company in companies" v-bind:key="company.id">
+      <tr v-for="company in companiesData" v-bind:key="company.id">
         <TableCell v-for="(value, key, i) in company"
             :key="i"
             :company="company"
             :content="value"
             :hasLink="key === 'name'"
         />
-        <TableCell :content="company.numberOfEmployees * company.subscriptionsPerEmployee" />
       </tr>
     </tbody>
   </table>
@@ -25,6 +24,18 @@ import TableCell from '../data-table/TableCell.vue';
 export default {
   components: {
     TableCell,
+  },
+  computed: {
+    companiesData() {
+      const { companies } = this;
+      const companiesData = companies;
+      // eslint-disable-next-line
+      companiesData.forEach((company) => (
+        // eslint-disable-next-line
+        company.totalSubscriptions = company.numberOfEmployees * company.subscriptionsPerEmployee
+      ));
+      return companiesData;
+    },
   },
   data() {
     return {
